1576f4387e4368d6421c42f3c5a398deaa4fac7d
Colin Binion <colin.binion@me.com>
2018-07-18 17:58:58 -0600

    libuv OS delegation

--- async.js
+++ async.js
@@ -2,13 +2,23 @@ const https = require('https');
 
 const start = Date.now();
 
-https
-  .request('https://www.google.com', res => {
-    res.on('data', () => {});
-    res.on('end', () => {
-      console.log('====================================');
-      console.log(Date.now() - start);
-      console.log('====================================');
-    });
-  })
-  .end();
+function doRequest() {
+  https
+    .request('https://www.google.com', res => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        console.log('====================================');
+        console.log(Date.now() - start);
+        console.log('====================================');
+      });
+    })
+    .end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();

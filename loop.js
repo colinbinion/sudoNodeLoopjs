143ea471ea50761e1670cb8b37b0be48ab1b5a78
Colin Binion <colin.binion@me.com>
2018-07-18 16:38:31 -0600

    event loop implementation

--- loop.js
+++ loop.js
@@ -1,9 +1,22 @@
 // node myFile.js
+
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+//new timers, tasks, operations are recorded from myFile running
 myFile.runContents();
 
-// entire body executes in on 'tick'
-while() {
-
+function shouldContinue() {
+  // check one: Any pending setTimeout, setInterval, setImmediate?
+  // check two: any pending OS tasks? (like a server listening to a port)
+  // check three: any pending long running operations? (like FS module)
+  return (
+    pendingTimers.length || pendingOSTasks.length || pendingOperations.length
+  );
 }
+
+// entire body executes in on 'tick'
+while (shouldContinue) {}
 
 // exit back to terminal

3a85d67182e983328d5b4a316e8bbf7d37907558
Colin Binion <colin.binion@me.com>
2018-07-18 17:16:11 -0600

    testing hyperthreading, cores, and threadpool

--- threads.js
+++ threads.js
@@ -12,3 +12,21 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('2:', Date.now() - start);
   console.log('====================================');
 });
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('====================================');
+  console.log('3:', Date.now() - start);
+  console.log('====================================');
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('====================================');
+  console.log('4:', Date.now() - start);
+  console.log('====================================');
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('====================================');
+  console.log('5:', Date.now() - start);
+  console.log('====================================');
+});

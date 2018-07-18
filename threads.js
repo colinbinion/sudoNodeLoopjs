8b58acab817b0aeb6aabaf53c2b0c018d6ecc39a
Colin Binion <colin.binion@me.com>
2018-07-18 17:08:28 -0600

    testing for single threads

--- threads.js
+++ threads.js
@@ -1,3 +1,14 @@
 const crypto = require('crypto');
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {});
+const start = Date.now();
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('====================================');
+  console.log('1:', Date.now() - start);
+  console.log('====================================');
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('====================================');
+  console.log('2:', Date.now() - start);
+  console.log('====================================');
+});

dc96b6fe0bb9a060efa00a0d9fcbad68ff778790
Colin Binion <colin.binion@me.com>
2018-07-19 20:33:18 -0600

    created/forked the node cluster children

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const cluster = require('cluster');
 
-console.log('====================================');
-console.log(cluster.isMaster);
-console.log('====================================');
+// qualify if file is being executed in master mode
+if (cluster.isMaster) {
+  // makes index.js to be executed *again* in child mode
+  cluster.fork();
 
-const express = require('express');
-const app = express();
+} else {
+  // determines that this is a child and act exclusively like a server
+  const express = require('express');
+  const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);

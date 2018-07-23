2b3587a4b4e201e3e38fb47ac3fbf42c91064371
Colin Binion <colin.binion@me.com>
2018-07-23 11:06:18 -0600

    refactoring for pm2 usage

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+// determines that this is a child and act exclusively like a server
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-// qualify if file is being executed in master mode
-if (cluster.isMaster) {
-  // makes index.js to be executed *again* in child mode
-  cluster.fork();
-  cluster.fork();
-
-} else {
-  // determines that this is a child and act exclusively like a server
-  const express = require('express');
-  const crypto = require('crypto');
-  const app = express();
-
-  // function doWork(duration) {
-  //   const start = Date.now();
-  //   while (Date.now() - start < duration) {}
-  // }
+// function doWork(duration) {
+//   const start = Date.now();
+//   while (Date.now() - start < duration) {}
+// }
 
 
 
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hi there');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi there');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('This is fast!!!!')
-  });
+app.get('/fast', (req, res) => {
+  res.send('This is fast!!!!')
+});
 
-  app.listen(3000);
-}
+app.listen(3000);

94071a385ee849654786e870789ba8f1f9670433
Colin Binion <colin.binion@me.com>
2018-07-23 12:03:07 -0600

    adding worker threads

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const cluster = require('cluster');
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const Worker = require('webworker-threads').Worker;
 
 
 app.get('/', (req, res) => {
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there');
+  const worker = new Worker(function () {
+    this.onmessage = () {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      this.postMessage(counter);
+    }
   });
+
+  worker.onmessage = function (myCounter) {
+    console.log('====================================');
+    console.log(myCounter);
+    console.log('====================================');
+  }
+
+  worker.postMessage();
+
 });
 
 app.get('/fast', (req, res) => {
